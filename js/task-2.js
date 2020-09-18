204dff2885b22d4d728905117e6c9d2ca9faf5fa
AndreyKovsh <64414142+AndreyKovsh@users.noreply.github.com>
2020-09-18 19:00:11 +0200

    ДЗ-2

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
+
+const contIngEl = document.querySelector('#ingredients');
+console.log(contIngEl);
+const products = ingredients.map(element => {
+  const itemsEl = document.createElement('li');
+  itemsEl.textContent = element;
+  return itemsEl;
+});
+console.log(products);
+contIngEl.append(...products);
